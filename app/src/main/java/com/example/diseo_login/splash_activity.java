94c17163c5aee89c2b0ff84f45d9bdacc557ac37
SQLazo <49886749+SQLazo@users.noreply.github.com>
2019-11-23 12:02:34 -0500

    Version 3

--- app/src/main/java/com/example/diseo_login/splash_activity.java
+++ app/src/main/java/com/example/diseo_login/splash_activity.java
@@ -10,22 +10,11 @@ import android.view.WindowManager;
 
 public class splash_activity extends Activity {
 
-    private final int DURACION_SPLASH = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_activity);
-
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        setContentView(R.layout.activity_splash_activity);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(splash_activity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            };
-        },DURACION_SPLASH);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
